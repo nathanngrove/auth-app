95abc054f120c0e856990258a2fc7817d8232c57
Nathan Grove <nathangrove08@gmail.com>
2022-05-13 20:39:34 -0700

    auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        if(!this->app->routesAreCached())
+		{
+			Passport::routes();
+		}
     }
 }
