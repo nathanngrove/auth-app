68c9a4b95b6ec51391679adceb83f161b40f038d
Nathan Grove <nathangrove08@gmail.com>
2022-05-13 22:22:21 -0700

    starting over

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,58 +16,3 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/authorization', function (Request $request) {  //Get authorization
-    $request->session()->put('state', $state = Str::random(40));
- 
-    $query = http_build_query([
-        'client_id' => '3',
-        'redirect_uri' => 'https://auth4500.herokuapp.com/callback',
-        'response_type' => 'code',
-        'scope' => '',
-        'state' => $state,
-    ]);
-
-    return redirect('https://auth4500.herokuapp.com/oauth/authorize?'.$query);
-})->name('authorization');
-
-
-
-Route::get('/callback', function (Request $request) {   //Get Token after authorization
-    $state = $request->session()->pull('state');
-    
-    if(strlen($state) > 0 && $state === $request->state) {
- 
-        $response = Http::asForm()->post('https://auth4500.herokuapp.com/oauth/token', [
-            'grant_type' => 'authorization_code',
-            'client_id' => '3',
-            'client_secret' => 'OTKhMsf6jtGZAS51A5RBJ22ZuVJBv8aY0yLhOFp8',
-            'redirect_uri' => 'https://auth4500.herokuapp.com/callback',
-            'code' => $request->code,
-        ]);
-        
-        $accessToken = $response->json()['access_token'];
-    
-        //Use the token to request data
-        $response = Http::withHeaders([
-            'Accept' => 'application/json',
-            'Authorization' => 'Bearer '.$accessToken,
-        ])->get('https://auth4500.herokuapp.com/api/users');
-        
-        return $response->json();
-        
-    } else {
-        return redirect()->route('authorization');
-    }
-});
-
-Route::get('/db-migrate', function () {
-    Artisan::call('migrate');
-    echo Artisan::output();
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
