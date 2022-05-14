2d261bbca0ed8f92f2db8443bb66629b7c699216
Nathan Grove <nathangrove08@gmail.com>
2022-05-13 22:24:05 -0700

    starting over

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/db-migrate', function () {
+    Artisan::call('migrate');
+    echo Artisan::output();
+});
